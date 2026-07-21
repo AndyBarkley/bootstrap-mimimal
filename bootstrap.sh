d7dc721330d59fb8993a646338eca82d0fdb8dad
Andy <andybarkley@yahoo.com>
2026-07-21 23:52:04 +0000

    updated to use andybob

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 apt update
 apt install -y git ansible
 
-mkdir -p /root/.ssh
-chmod 700 /root/.ssh
+mkdir -p /home/andybob/.ssh
+chmod 700 /home/andybob/.ssh
 
 if [ ! -f /root/.ssh/id_ansible_pull ]; then
-    ssh-keygen -t ed25519 -f /root/.ssh/id_ansible_pull -C "ansible-pull" -N ""
+    ssh-keygen -t ed25519 -f /home/andybob/.ssh/id_ansible_pull -C "ansible-pull" -N ""
 fi
 
 echo "==== PUBLIC KEY FOR GITHUB DEPLOY KEY ===="
-cat /root/.ssh/id_ansible_pull.pub
+cat /home/andybob/.ssh/id_ansible_pull.pub
 echo "=========================================="
 echo "Add this key as a READ-ONLY Deploy Key in your GitHub repo."
 echo "Press enter when ready."
@@ -21,5 +21,5 @@ read
 ANSIBLE_SSH_ARGS="-i /root/.ssh/id_ansible_pull" \
 ansible-pull \
   -U https://github.com/AndyBarkley/bootstrap-mimimal.git \
-  -d /root/.ansible/pull/bootstrap-minimal  \
+  -d /home/andybob/.ansible/pull/bootstrap-minimal  \
   site.yml
