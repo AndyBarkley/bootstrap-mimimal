b6405f9d150f8f8199b6c755488a02648fcfa7c0
Andy <andybarkley@yahoo.com>
2026-07-21 23:55:58 +0000

    updated check before creating the ssh key

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,7 @@ apt install -y git ansible
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 
-if [ ! -f /root/.ssh/id_ansible_pull ]; then
+if [ ! -f ~/.ssh/id_ansible_pull ]; then
     ssh-keygen -t ed25519 -f ~/.ssh/id_ansible_pull -C "ansible-pull" -N ""
 fi
 
